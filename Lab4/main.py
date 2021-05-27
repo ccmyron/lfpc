ac7e97a453118f0ce8dda909078035430336f9ce
Miron Cristian Catalin <miron.cristiancatalin@gmail.com>
2021-05-27 19:18:49 +0300

    some minor changes

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,20 +1,22 @@
 from test import stringcheck, grammarcheck
 
-c = int(input("Enter the number of LHS variables..\n"))
+if __name__ == "__main__":
 
-for i in range(c):
-    if grammarcheck(i):
-        t = True
+    c = int(input("Enter the number of LHS variables..\n"))
+
+    for i in range(c):
+        if grammarcheck(i):
+            t = True
+        else:
+            t = False
+            break
+
+    if t:
+        print("Grammar is accepted")
+        if (stringcheck()):
+            print("String is accepted")
+        else:
+            print("String is not accepted")
+
     else:
-        t = False
-        break
-
-if t:
-    print("Grammar is accepted")
-    if (stringcheck()):
-        print("String is accepted")
-    else:
-        print("String is not accepted")
-
-else:
-    print("Grammar is not accepted ")
+        print("Grammar is not accepted ")
