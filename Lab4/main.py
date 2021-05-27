5c442d7bd93bbbb5ab44dab873d6ffc26dc03cfd
Miron Cristian Catalin <miron.cristiancatalin@gmail.com>
2021-05-27 19:16:34 +0300

    Added 4th lab

---
+++ Lab4/main.py
@@ -0,0 +1,20 @@
+from test import stringcheck, grammarcheck
+
+c = int(input("Enter the number of LHS variables..\n"))
+
+for i in range(c):
+    if grammarcheck(i):
+        t = True
+    else:
+        t = False
+        break
+
+if t:
+    print("Grammar is accepted")
+    if (stringcheck()):
+        print("String is accepted")
+    else:
+        print("String is not accepted")
+
+else:
+    print("Grammar is not accepted ")
